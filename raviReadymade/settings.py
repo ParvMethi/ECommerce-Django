94be03b88f7b1742ef0d9a276df24d5523cd6d4b
Parv Methi <parv.methi@netmeds.com>
2023-03-27 18:42:16 +0530

    Product_Model

--- raviReadymade/settings.py
+++ raviReadymade/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'category',
     'accounts',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,10 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'raviReadymade/static',
 ]
+
+# media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- raviReadymade/urls.py
+++ raviReadymade/urls.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
 from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home')
-]
+    path('', home, name='home'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

eaa02e3c043d115dcfcf54e30ca7af3d1d35ab96
Parv Methi <parv.methi@netmeds.com>
2023-04-18 17:35:28 +0530

    models_review_rating

--- store/models.py
+++ store/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.urls import reverse
+from accounts.models import Account
 from category.models import Category
 # Create your models here.
+
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -43,3 +45,18 @@ class Variation(models.Model):
 
     def __str__(self) -> str:
         return self.variation_value
+    
+
+class ReviewRating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.subject

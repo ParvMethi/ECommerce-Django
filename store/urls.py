285199bafdbe8600bccf12211fd5421834233334
Parv Methi <parv.methi@netmeds.com>
2023-04-19 23:55:35 +0530

    storing_reviews

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from store.views import store, product_detail, search
+from store.views import store, product_detail, search, submit_review
 
 urlpatterns = [
     path('', store, name='store'),
     path('category/<slug:category_slug>/', store, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>/', product_detail, name='product_detail'),
-    path('search/', search, name='search')
+    path('search/', search, name='search'),
+    path('submit_review/<int:product_id>/', submit_review, name='submit_review'),
 ]
